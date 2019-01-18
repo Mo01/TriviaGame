822bd0c31788c67ea818fac42a6be8f680285d31
Mo <moh.alneama@yahoo.com>
2019-01-17 18:43:16 -0600

    adding rest for each button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
 
 
-// To start the game
-//$("#start").click(start);
 
 
 var myQuestions = [
@@ -45,78 +43,166 @@ var myQuestions = [
         },
         correctAnswer: 'Hummus'
     }
-    
+
 
 ];
+//****************************************/
+//reset
+// function reset() {
+//     $('#onGoing').css('display', 'none');
+//     setTimeout(function () {
+//         displayQuestion(++i);
+//     }, 3000);
+//     clearTimeout(timeOut);
+// }
 //*********************************************************************//
 //  Display Qustion
 
 var correct = 0;
 var incorrect = 0;
-function displayQuestion(i) {
-    var timeOut = setTimeout(displayQuestion.bind(null, i + 1), 10000);
-    $("#qustions").text(myQuestions[i].question);
-    $("#answA").text(myQuestions[i].answers.a);
-    $("#answA").val(myQuestions[i].answers.a);
-    $("#answB").text(myQuestions[i].answers.b);
-    $("#answB").val(myQuestions[i].answers.b);
-    $("#answC").text(myQuestions[i].answers.c);
-    $("#answC").val(myQuestions[i].answers.c);
-    $("#answD").text(myQuestions[i].answers.d);
-    $("#answD").val(myQuestions[i].answers.d);
-    $("#answA").off('click').on('click', function() {
-        if($(this).val() === myQuestions[i].correctAnswer) {
-            ++correct;
-        }
-        else {
-            ++incorrect;
-        }
-        clearTimeout(timeOut);
-        displayQuestion(++i);
-    });
-    $("#answB").off('click').on('click', function() {
-        if($(this).val() === myQuestions[i].correctAnswer) {
-            ++correct;
-        }
-        else {
-            ++incorrect;
-        }
-        clearTimeout(timeOut);
-        displayQuestion(++i);
-    });
-    $("#answC").off('click').on('click', function() {
-        if($(this).val() === myQuestions[i].correctAnswer) {
-            ++correct;
-        }
-        else {
-            ++incorrect;
-        }
-        clearTimeout(timeOut);
-        displayQuestion(++i);
-    });
-    $("#answD").off('click').on('click', function() {
-        if($(this).val() === myQuestions[i].correctAnswer) {
-            ++correct;
-        }
-        else {
-            ++incorrect;
-        }
-        clearTimeout(timeOut);
-        displayQuestion(++i);
-    });
-}
+var unanswered = 0;
 
+function displayQuestion(i) {
+    $('#onGoing').css('display', 'block');
+    if (i >= myQuestions.length) {
+        $("#onGoing").css("display", "none");
+        $("#end").css("display", "block");
+        $("#correct").text("correct = " + correct);
+        $("#incorrect").text("incorrect = " + incorrect);
+        $("#unanswered").text("unanswered = " + unanswered);
+
+    }
+    else {
+        //timer to display
+        var remainingTime = 10;
+        $("#timer").text(remainingTime);
+        var counter = setInterval(function () {
+            if (remainingTime >= 0) {
+                remainingTime--;
+                $("#timer").text(remainingTime);
+            }
+            else {
+                clearInterval(counter);
+            }
+        }, 1000);
+
+        // Timer to change pages
+        var timeOut = setTimeout(function () {
+            ++unanswered;
+            //reset();
+            $('#onGoing').css('display', 'none');
+            setTimeout(function () {
+                displayQuestion(++i);
+            }, 3000);
+            clearTimeout(timeOut);
+            displayQuestion.bind(null, i + 1)();
+        }, 10000);
+
+        // display questions
+        $("#qustions").text(myQuestions[i].question);
+        $("#answA").text(myQuestions[i].answers.a);
+        $("#answA").val(myQuestions[i].answers.a);
+        $("#answB").text(myQuestions[i].answers.b);
+        $("#answB").val(myQuestions[i].answers.b);
+        $("#answC").text(myQuestions[i].answers.c);
+        $("#answC").val(myQuestions[i].answers.c);
+        $("#answD").text(myQuestions[i].answers.d);
+        $("#answD").val(myQuestions[i].answers.d);
+
+        // handle events
+        $("#answA").off('click').on('click', function () {
+            if ($(this).val() === myQuestions[i].correctAnswer) {
+                ++correct;
+                // reset();
+                $('#onGoing').css('display', 'none');
+                setTimeout(function () {
+                    displayQuestion(++i);
+                }, 3000);
+                clearTimeout(timeOut);
+            }
+            else {
+                ++incorrect;
+                //reset();
+                $('#onGoing').css('display', 'none');
+                setTimeout(function () {
+                    displayQuestion(++i);
+                }, 3000);
+                clearTimeout(timeOut);
+            }
+            clearInterval(counter);
+
+        });
+        $("#answB").off('click').on('click', function () {
+            if ($(this).val() === myQuestions[i].correctAnswer) {
+                ++correct;
+                //reset();
+                $('#onGoing').css('display', 'none');
+                setTimeout(function () {
+                    displayQuestion(++i);
+                }, 3000);
+                clearTimeout(timeOut);
+            }
+            else {
+                ++incorrect;
+                //reset();
+                $('#onGoing').css('display', 'none');
+                setTimeout(function () {
+                    displayQuestion(++i);
+                }, 3000);
+                clearTimeout(timeOut);
+            }
+            clearInterval(counter);
+            //clearTimeout(timeOut);
+            //displayQuestion(++i);
+        });
+        $("#answC").off('click').on('click', function () {
+            if ($(this).val() === myQuestions[i].correctAnswer) {
+                ++correct;
+                // reset();
+                $('#onGoing').css('display', 'none');
+                setTimeout(function () {
+                    displayQuestion(++i);
+                }, 3000);
+                clearTimeout(timeOut);
+            }
+            else {
+                ++incorrect;
+                // reset();
+                $('#onGoing').css('display', 'none');
+                setTimeout(function () {
+                    displayQuestion(++i);
+                }, 3000);
+                clearTimeout(timeOut);
+            }
+            clearInterval(counter);
+            // clearTimeout(timeOut);
+            // displayQuestion(++i);
+        });
+        $("#answD").off('click').on('click', function () {
+            if ($(this).val() === myQuestions[i].correctAnswer) {
+                ++correct;
+                //  reset();
+                $('#onGoing').css('display', 'none');
+                setTimeout(function () {
+                    displayQuestion(++i);
+                }, 3000);
+                clearTimeout(timeOut);
+            }
+            else {
+                ++incorrect;
+                //   reset();
+                $('#onGoing').css('display', 'none');
+                setTimeout(function () {
+                    displayQuestion(++i);
+                }, 3000);
+                clearTimeout(timeOut);
+            }
+            clearInterval(counter);
+            // clearTimeout(timeOut);
+            // displayQuestion(++i);
+        });
+    }
+}
 var i = 0;
 displayQuestion(i);
 
-//******************************************************************//
-// Timer
-//var timeLeft = 5;
-//var timerId = setInterval(countdown, 1000);
-
-
-        //doSomething**************************//
-   // } 
-//}
-//}
-//*****************************************************************//
